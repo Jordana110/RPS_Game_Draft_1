c65bc3e9c0d2d085a65a8d40e342e60024aa1f45
Jordana110 <jordanadauer@gmail.com>
2023-07-14 12:02:37 +0200

    RPS_Game_Draft_1

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+//const for rock-paper-scissors
+//rounds 5-10-15-20 const for that too
+// 2 player and point fields user-comp
+//click of icon starts game
+//restart resets game
+
+const roundFive = document.querySelector("#five");
+const roundTen = document.querySelector("#ten");
+const roundFifteen = document.querySelector("#fifteen");
+const roundTwenty = document.querySelector("#twenty");
+
+const resultU = document.querySelector("#resultUser");
+const resultC = document.querySelector("#resultComp");
+
+const textD = document.querySelector("textdescription");
+
+const rockIcon = document.querySelector("rock");
+const paperIcon = document.querySelector("paper");
+const scissorIcon = document.querySelector("scissors");
+
+const resetB = document.querySelector("#resetbutton");
+const textDescription = document.querySelector("textd");
